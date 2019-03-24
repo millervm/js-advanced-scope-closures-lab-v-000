b1be3f5b71bbff373325ab5dd515da53f3a104b0
Veronica Miller <vmcmiller@gmail.com>
2019-03-24 21:10:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function produceDrivingRange(range) {
-  return foo(start, end) {
+  //return foo(start, end) {
     //if (end < (start + range)) { return `within range by ${abs(end - (start + range))}`; };
     //else { return `${abs(end - (start + range))} out of range`};
-  };
+  //};
 };
