7d2282070a47f0a5a27647c7abe661ca062fb9e5
Veronica Miller <vmcmiller@gmail.com>
2019-03-24 21:43:11 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function produceDrivingRange(range) {
+  return function(startString, endString) {
+    let start = Number(startString.match(/\d+/g)[0]);
+    let end = Number(endString.match(/\d+/g)[0]);
+    if ((start + range) > end) { return `within range by ${Math.abs(end - (start + range))}`; }
+    else { return `${Math.abs(end - (start + range))} blocks out of range`; };
+  };
+};
+
+function produceTipCalculator(tip) {
+  return function(fare) { return tip * fare; };
+};
+
+function createDriver() {
+  let driverId = 0;
+
+  return class {
+    constructor(name) {
+      this.id = ++driverId;
+      this.name = name;
+    };
+  };
+
+};
